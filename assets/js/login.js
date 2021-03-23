c8ca3082f8b6085229e59eca06d360c05dda7d64
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2021-03-23 12:51:09 +0100

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,1 +1,22 @@
+ function getInputValue(){
+   // Selecting the input element and get its value 
+   var inputPrenom = document.getElementById("prenom").value;
+   var inputMDP = document.getElementById("MDP").value;
 
+   var inputComplete = `${inputPrenom}_${inputMDP}`
+
+   // Displaying the value
+   if (inputPrenom === "" || inputMDP === "" || inputPrenom === "" && inputMDP === "") {
+     alert("L'identifiant ou le mot de passe sont invalident");
+   }
+   else {
+     if (inputComplete === `DDPStaff_Staff`) {
+       alert("Bonjour à toi le staff.");
+       window.location.replace("../index.html");
+     }
+     else if (inputComplete !== `DDPStaff_Staff`) {
+       alert(`Bonjour à toi ${inputPrenom}`);
+       window.location.replace("../index.html");
+     }
+   }
+ }
