3fd36a44f03619ecbdec953fcad1615413de70be
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2021-03-23 13:24:11 +0100

    Add files via upload

---
+++ assets/js/signin.js
@@ -0,0 +1,23 @@
+function getInputValue(){
+    // Selecting the input element and get its value 
+    var inputPrenom = document.getElementById("identifiant").value;
+    var inputMDP = document.getElementById("MDP").value;
+  
+    var inputComplete = `${inputPrenom}_${inputMDP}`
+  
+    // Displaying the value
+    if (inputPrenom === "" || inputMDP === "" || inputPrenom === "" && inputMDP === "") {
+      alert("L'identifiant ou le mot de passe sont invalides.");
+    }
+    else {
+      if (inputComplete === `DDPStaff_Staff`) {
+        alert("Bonjour à toi le staff.");
+        window.location.replace("../index.html");
+      }
+      else if (inputComplete !== `DDPStaff_Staff`) {
+        alert(`Bonjour à toi ${inputPrenom}`);
+        window.location.replace("../index.html");
+      }
+    }
+  }
+  
